eb11cc7dbdb5f94fee687fb5db03c08336f88d96
Julian Sim <54722630+juliansim1729@users.noreply.github.com>
2022-07-06 11:38:24 -0400

    Adding relevant work

---
+++ modified_funcs/attr_edgelist.R
@@ -0,0 +1,22 @@
+
+attr_edgelist <- function(dat, at, del, susattr = NULL, infattr = NULL) {
+    
+    # process susceptible attributes
+    if (!is.null(susattr) & !is.null(del)) {
+        req_args <- get_attr_list(dat, susattr)
+        for (i in 1:length(req_args)) {
+            del[paste0('sus.', names(req_args[i]))] <- 
+                req_args[[i]][del$sus]
+        }
+    }
+    
+    # process infected attributes
+    if (!is.null(infattr) & !is.null(del)) {
+        req_args <- get_attr_list(dat, infattr)
+        for (i in 1:lxength(req_args)) {
+            del[paste0('inf.', names(req_args[i]))] <- 
+                req_args[[i]][del$sus]
+        }
+    }
+    return(del)
+}
