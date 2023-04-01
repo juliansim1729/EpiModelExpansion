e726e5dc628e8b90c41195772ea9f86fc64d8c33
Julian Sim <54722630+juliansim1729@users.noreply.github.com>
2023-04-01 03:27:09 -0400

    aaaa

--- Final/main_pds.R
+++ Final/main_pds.R
@@ -9,7 +9,7 @@ library("ndtv")
 data(faux.mesa.high)
 
 # Params
-p_numIter = 10
+p_numIter = 1
 
 # Modules ----
 
@@ -30,11 +30,8 @@ mod_onStep <- function(dat, at) {
   
   if (nElig > 0) {
     # immunity
-    immunity[idsNotRec] <- ifelse(immunity[idsNotRec] > 0, immunity[idsNotRec] - 
-                                    imm.nRecMod*imm.decay, immunity[idsNotRec])
-    immunity[idsRec] <- ifelse(immunity[idsRec] > 0, immunity[idsRec] - 
-                                 imm.decay, immunity[idsRec])
-    immunity[idsElig] <- ifelse(immunity[idsElig] > 0, immunity[idsElig], 0)
+    immunity[idsElig] <- ifelse(immunity[idsElig] > 0, immunity[idsElig]/2, immunity[idsElig])
+    immunity[idsElig] <- ifelse(immunity[idsElig] > 0.01, immunity[idsElig], 0)
     
     # age
     age[idsElig] <- age[idsElig] + ( 1 / 365 )
@@ -189,7 +186,7 @@ mod_progress_rs <- function(dat, at) {
 nw <- faux.mesa.high
 
 fmh_grades <- get.node.attr(faux.mesa.high, 'Grade')
-fmh_schedule <- trad_classgen(fmh_grades, 12)
+fmh_schedule <- trad_classgen(fmh_grades, 20)
 
 for (period in 1:9) {
   fmh_pdV <- paste0('pd', period)
@@ -216,7 +213,7 @@ for (n in 1:p_numIter) {
                         nodematch("pd6") + nodematch("pd7") + nodematch("pd8") +
                         nodematch("pd9") + nodematch("lunch") + edgecov(as.matrix(faux.mesa.high)) +
                         edgecov(nocontact_mat),
-                      coef = c(0, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, Inf, -Inf),
+                      coef = c(-1.0986, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, -0.1, Inf, -Inf),
                       control = control.simulate(MCMC.burnin = 1000000, MCMC.interval = 100))
   
   target.stats <- attr(fmh_sim, "stats")[1:11]
@@ -224,7 +221,7 @@ for (n in 1:p_numIter) {
   est <- netest(nw, formation = ergm_formula, target.stats = target.stats, 
                 coef.diss = coef.diss, coef.form = c(Inf, -Inf))
   
-  nsteps <- 45
+  nsteps <- 270
   control <- control.net(type = NULL, nsteps = nsteps, nsims = 1, 
                          infection.FUN = NULL, recovery.FUN = NULL, 
                          onStep.FUN = mod_onStep, initialize.FUN = e_initialize.net, 
@@ -235,9 +232,9 @@ for (n in 1:p_numIter) {
                          resimulate.network = FALSE, verbose.int = 0)
   
   ### 2.6: Network Parameters
-  param <- param.net(ise.prob = 0.3,
-                     ei.rate = 0.05, ir.rate = 0.03, rs.rate = 0.001,
-                     imm.gain = 2, imm.decay = 0.1, imm.nRecMod = 2,
+  param <- param.net(ise.prob = 0.25,
+                     ei.rate = 0.1, ir.rate = 0.05, rs.rate = 0.02,
+                     imm.gain = 1, imm.decay = 0.1, imm.nRecMod = 2,
                      departure.disease.mult = 100,
                      arrival.rate = 1/(365*85),
                      act.rate = 1)
@@ -260,6 +257,8 @@ for (n in 1:p_numIter) {
   print(paste(n, "/", p_numIter, " trials completed.", sep = ""))
 }
 
+sim1 <- sim
+
 sim$epi$s.num <- dflist_plot$s.num
 sim$epi$e.num <- dflist_plot$e.num
 sim$epi$i.num <- dflist_plot$i.num
@@ -268,3 +267,65 @@ sim$epi$num <- dflist_plot$num
 sim$control$nsims <- p_numIter
 
 plot(sim)
+
+sim <- sim1
+
+## 3: Plots & Animations
+ntwk <- get_network(sim)
+ntwk <- n_size_tea(ntwk, "teimmunity")
+ntwk <- e_color_tea(ntwk)
+ntwk_light <- e_color_tea(ntwk, alpha = 0.15)
+
+layout(matrix(c(1,2,3,4,5,6),nrow=2,ncol=3,byrow=TRUE))
+# 
+# plot(network.collapse(ntwk, at = 1), vertex.col='ndtvcol',
+#      main = paste0('simulated network at t=', 1),
+#      vertex.cex = 1.5, edge.lwd = 2)
+# plot(network.collapse(ntwk, at = floor(nsteps/5)), vertex.col='ndtvcol',
+#      main = paste0('simulated network at t=', floor(nsteps/5)),
+#      vertex.cex = 1.5, edge.lwd = 2)
+# plot(network.collapse(ntwk, at = floor(2*nsteps/5)), vertex.col='ndtvcol',
+#      main = paste0('simulated network at t=', floor(2*nsteps/5)),
+#      vertex.cex = 1.5, edge.lwd = 2)
+# plot(network.collapse(ntwk, at = floor(3*nsteps/5)), vertex.col='ndtvcol',
+#      main = paste0('simulated network at t=', floor(3*nsteps/5)),
+#      vertex.cex = 1.5, edge.lwd = 2)
+# plot(network.collapse(ntwk, at = floor(4*nsteps/5)), vertex.col='ndtvcol',
+#      main = paste0('simulated network at t=', floor(4*nsteps/5)),
+#      vertex.cex = 1.5, edge.lwd = 2)
+# plot(network.collapse(ntwk, at = nsteps), vertex.col='ndtvcol',
+#      main = paste0('simulated network at t=', nsteps),
+#      vertex.cex = 1.5, edge.lwd = 2)
+
+layout(1)
+
+plot(sim, type = "network", at = 1, sims = "mean", col.status = TRUE, main = "Prevalence at t = 1")
+plot(sim, type = "network", at = 90, sims = "mean", col.status = TRUE, main = "Prevalence at t = 1")
+plot(sim, type = "network", at = 180, sims = "mean", col.status = TRUE, main = "Prevalence at t = 1")
+plot(sim, type = "network", at = 270, sims = "mean", col.status = TRUE, main = "Prevalence at t = 1")
+
+
+### 3.2: Animations
+
+# render an animation of the network
+render.par <- list(tween.frames=5,show.time=TRUE,
+                   show.stats=NULL )# ,extraPlotCmds = expression(
+#  legend(locator(1), legend = slice%v%'testatus')))
+plot.par <- list(mar = c(0, 0, 0, 0))
+
+compute.animation(ntwk,animation.mode = 'MDSJ', chain.direction = 'reverse', verbose=FALSE)
+
+# TODO: floating legend
+render.d3movie(
+  ntwk,
+  vertex.tooltip = function(slice){paste('name:',slice%v%'vertex.names','<br>',
+                                         'status:', slice%v%'testatus', '<br>',
+                                         'immunity:', round(slice%v%'teimmunity', 3), '<br>',
+                                         'age:', round(slice%v%'teage', 3), '<br>',
+                                         'sex:', slice%v%'Sex')},
+  d3.options=list(animationDuration=2000,enterExitAnimationFactor=0.5),
+  render.par = render.par, plot.par = plot.par,
+  vertex.cex = "ndtvcex", vertex.col = "ndtvcol", vertex.border = "lightgrey",
+  label.cex=0.8,label.col="black", verbose = FALSE,
+  main = "Simulated Spread of SEIR with Tracked Immunity on a Network",
+  displaylabels = TRUE)
